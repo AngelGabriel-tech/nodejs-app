6bc494bf4a560e4b590f862fd87f5091d6171659
AngelGabriel-tech <68243820+AngelGabriel-tech@users.noreply.github.com>
2025-03-27 00:44:49 +0100

    Update server.test.js

--- server.test.js
+++ server.test.js
@@ -1,6 +1,18 @@
 const request = require("supertest");
 const app = require("./server");
 
+let server;
+
+beforeAll(() => {
+  // Start the server before running tests
+  server = app.listen(3000);
+});
+
+afterAll(() => {
+  // Close the server after tests are done
+  server.close();
+});
+
 describe("GET /", () => {
   it("should return 'Hello, Jenkins!'", async () => {
     const res = await request(app).get("/");
